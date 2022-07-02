2e5fd2daca50577e514572a84ed513e3d3f16e5b
Livy <42135339+Livy-17@users.noreply.github.com>
2022-07-02 17:57:01 +0100

    commit

---
+++ components/Layout.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import Head from 'next/head';
+import Header from './Header';
+import Footer from './Footer';
+
+const Layout = ({ children }) => {
+  return ( 
+    <>
+      <Head>
+        <title>WhatToWatch</title>
+      </Head>
+      <header>
+        <Header />
+      </header>
+      <main>
+        { children }
+      </main>
+      <footer>
+        <Footer />
+      </footer>
+    </>
+   );
+}
+ 
+export default Layout;
