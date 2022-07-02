af752be6af2dcf41cf22572d4110a57535b5a03b
Livy <42135339+Livy-17@users.noreply.github.com>
2022-07-02 19:53:01 +0100

    commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 import '../styles/globals.css';
 import Layout from '../components/Layout';
 import '../components/CarouselMain/css/emblaMain.css';
+import '../components/CarouselSub/css/emblaSub.css';
 
 function MyApp({ Component, pageProps }) {
   return (
--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,19 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import EmblaCarouselMain from '../components/CarouselMain/js/EmblaCarouselMain';
+import EmblaCarouselSub from '../components/CarouselSub/js/EmblaCarouselSub';
 
 const SLIDE_COUNT_MAIN = 5;
 const slides_main = Array.from(Array(SLIDE_COUNT_MAIN).keys());
+const SLIDE_COUNT_SUB = 5;
+const slides_sub = Array.from(Array(SLIDE_COUNT_SUB).keys());
 
 export default function Home() {
   return (
     <>
       <div>
-        <EmblaCarouselMain slides={slides_main} />
+        {/* <EmblaCarouselMain slides={slides_main} /> */}
+        <EmblaCarouselSub slides={slides_sub} /> 
       </div>
     </>
   )
