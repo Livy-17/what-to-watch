8d2edb4a50f6411515755e0a3219b385acd87721
Livy <42135339+Livy-17@users.noreply.github.com>
2022-07-02 19:41:05 +0100

    commit

--- components/Header.jsx
+++ components/Header.jsx
@@ -3,12 +3,11 @@ import { HiOutlineSearch } from 'react-icons/hi';
 
 const Header = () => {
   return ( 
-    <div className='flex w-full h-10 bg-red-100 items-center justify-between pl-10 pr-10'>
+    <div className='flex w-1900 h-10 bg-red-100 items-center justify-between pl-10 pr-10'>
       <p>WhatToWatch</p>
       <div className='flex flex-row items-center justify-center'>
         <input />
-        {/* <HiOutlineSearch className='ml-1 mr-1'/> */}
-        <div>Hi</div>
+        <HiOutlineSearch className='ml-1 mr-1'/>
       </div>
     </div>
    );
--- pages/index.js
+++ pages/index.js
@@ -8,8 +8,9 @@ const slides_main = Array.from(Array(SLIDE_COUNT_MAIN).keys());
 export default function Home() {
   return (
     <>
-      <div>123</div>
-      <EmblaCarouselMain slides={slides_main} />
+      <div>
+        <EmblaCarouselMain slides={slides_main} />
+      </div>
     </>
   )
 }
