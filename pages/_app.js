c4f8ea21061b805916fb907717560b21b2ab6a52
Livy <42135339+Livy-17@users.noreply.github.com>
2022-07-03 15:45:11 +0100

    1

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,5 @@
 import '../styles/globals.css';
 import Layout from '../components/Layout';
-import '../components/CarouselMain/css/emblaMain.css';
-import '../components/CarouselSub/css/emblaSub.css';
-import '../components/CarouselTest/css/emblaTest.css';
 
 function MyApp({ Component, pageProps }) {
   return (
--- pages/index.js
+++ pages/index.js
@@ -1,25 +1,14 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import EmblaCarouselMain from '../components/CarouselMain/js/EmblaCarouselMain';
-import EmblaCarouselSub from '../components/CarouselSub/js/EmblaCarouselSub';
-import EmblaCarouselTest from '../components/CarouselTest/js/emblaCarouselTest';
 import { fetch_by_country_service_page } from '../uilts/fetchAPI';
 import { baseURL } from '../uilts/fetchAPI';
-
-const slides_main = Array.from(Array(5).keys());
-const slides_sub = Array.from(Array(5).keys());
-const slides_sub_us_netflix = Array.from(Array(16).keys());
 
 export default function Home({ data_us_netfilx }) {
   const images_us_netflix = data_us_netfilx.map(item => item.posterURLs['500']);
   const index_images_us_netflix = index => images_us_netflix[index % images_us_netflix.length];
-  console.log(images_us_netflix)
   return (
     <>
       <div>
-        <EmblaCarouselMain slides={slides_main} />
-        {/* <EmblaCarouselSub slides={slides_sub_us_netflix} mediaSubByIndex={index_images_us_netflix} />  */}
-        <EmblaCarouselTest slides={slides_sub_us_netflix} mediaSubByIndex={index_images_us_netflix} /> 
       </div>
     </>
   )
