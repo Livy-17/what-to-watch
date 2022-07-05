d60b8b68acf9cad6131b1d75ce51d2f972b8ec5e
Livy <42135339+Livy-17@users.noreply.github.com>
2022-07-05 15:07:01 +0100

    1

--- pages/countries/us.js
+++ pages/countries/us.js
@@ -5,112 +5,7 @@ import { baseURL, fetch_by_country_service_page, fetch_service_by_country } from
 import CarouselMain from '../../components/CarouselMain';
 import CarouselSub from '../../components/CarouselSub';
 
-export default function Home({ data_us_netfilx }) {
-
-  const [country, setCountry] = useState('us');
-  const [isLoading, setIsLoading] = useState(false);
-
-  const [serviceByCountry, setServiceByCountry] = useState({});
-
-  const [DataNetflix, setDataNetflix] = useState(null);
-  const [DataPrime, setDataPrime] = useState(null);
-  const [DataDisney, setDataDisney] = useState(null);
-  const [DataHbo, setDataHbo] = useState(null);
-  const [DataHulu, setDataHulu] = useState(null);
-  const [DataPeacock, setDataPeacock] = useState(null);
-  const [DataParamount, setDataParamount] = useState(null);
-  const [DataStarz, setDataStarz] = useState(null);
-  const [DataShowtime, setDataShowtime] = useState(null);
-  const [DataApple, setDataApple] = useState(null);
-  const [DataMubi, setDataMubi] = useState(null);
-
-  useEffect(() => {
-
-    setIsLoading(true);
-
-    fetch_service_by_country().then(obj => setServiceByCountry(obj))
-
-  }, []);
-
-  useEffect(() => {
-
-    setIsLoading(true);
-
-    const runFetch = async () => {
-
-      if (serviceByCountry?.netflix?.includes(country)) {
-        const array1 = await fetch_by_country_service_page(`${baseURL}/search/pro`, country, 'netflix', 1);
-        const array2 = await fetch_by_country_service_page(`${baseURL}/search/pro`, country, 'netflix', 2);
-        setDataNetflix(array1.concat(array2));
-      }
-
-      if (serviceByCountry?.prime?.includes(country)) {
-        const array1 = await fetch_by_country_service_page(`${baseURL}/search/pro`, country, 'prime', 1);
-        const array2 = await fetch_by_country_service_page(`${baseURL}/search/pro`, country, 'prime', 2);
-        setDataPrime(array1.concat(array2));
-      }
-
-      if (serviceByCountry?.disney?.includes(country)) {
-        const array1 = await fetch_by_country_service_page(`${baseURL}/search/pro`, country, 'disney', 1);
-        const array2 = await fetch_by_country_service_page(`${baseURL}/search/pro`, country, 'disney', 2);
-        setDataDisney(array1.concat(array2));
-      }
-
-      if (serviceByCountry?.hbo?.includes(country)) {
-        const array1 = await fetch_by_country_service_page(`${baseURL}/search/pro`, country, 'hbo', 1);
-        const array2 = await fetch_by_country_service_page(`${baseURL}/search/pro`, country, 'hbo', 2);
-        setDataHbo(array1.concat(array2));
-      }
-
-      if (serviceByCountry?.hulu?.includes(country)) {
-        const array1 = await fetch_by_country_service_page(`${baseURL}/search/pro`, country, 'hulu', 1);
-        const array2 = await fetch_by_country_service_page(`${baseURL}/search/pro`, country, 'hulu', 2);
-        setDataHulu(array1.concat(array2));
-      }
-
-      if (serviceByCountry?.peacock?.includes(country)) {
-        const array1 = await fetch_by_country_service_page(`${baseURL}/search/pro`, country, 'peacock', 1);
-        const array2 = await fetch_by_country_service_page(`${baseURL}/search/pro`, country, 'peacock', 2);
-        setDataPeacock(array1.concat(array2));
-      }
-
-      if (serviceByCountry?.paramount?.includes(country)) {
-        const array1 = await fetch_by_country_service_page(`${baseURL}/search/pro`, country, 'paramount', 1);
-        const array2 = await fetch_by_country_service_page(`${baseURL}/search/pro`, country, 'paramount', 2);
-        setDataParamount(array1.concat(array2));
-      }
-
-      if (serviceByCountry?.starz?.includes(country)) {
-        const array1 = await fetch_by_country_service_page(`${baseURL}/search/pro`, country, 'starz', 1);
-        const array2 = await fetch_by_country_service_page(`${baseURL}/search/pro`, country, 'starz', 2);
-        setDataStarz(array1.concat(array2));
-      }
-
-      if (serviceByCountry?.showtime?.includes(country)) {
-        const array1 = await fetch_by_country_service_page(`${baseURL}/search/pro`, country, 'showtime', 1);
-        const array2 = await fetch_by_country_service_page(`${baseURL}/search/pro`, country, 'showtime', 2);
-        setDataShowtime(array1.concat(array2));
-      }
-
-      if (serviceByCountry?.apple?.includes(country)) {
-        const array1 = await fetch_by_country_service_page(`${baseURL}/search/pro`, country, 'apple', 1);
-        const array2 = await fetch_by_country_service_page(`${baseURL}/search/pro`, country, 'apple', 2);
-        setDataApple(array1.concat(array2));
-      }
-
-      if (serviceByCountry?.mubi?.includes(country)) {
-        const array1 = await fetch_by_country_service_page(`${baseURL}/search/pro`, country, 'mubi', 1);
-        const array2 = await fetch_by_country_service_page(`${baseURL}/search/pro`, country, 'mubi', 2);
-        setDataMubi(array1.concat(array2));
-      }
-
-    }
-
-    runFetch();
-
-    setIsLoading(false);
-
-  }, [serviceByCountry, country])
+export default function Home({ DataNetflix, DataPrime, DataDisney, DataHbo, DataHulu, DataPeacock, DataParamount, DataStarz, DataShowtime, DataApple, DataMubi }) {
 
   if (DataNetflix) {var imagesNetflix = DataNetflix.map(item => item.posterURLs['500']);}
   if (DataPrime) {var imagesPrime = DataPrime.map(item => item.posterURLs['500']);}
@@ -123,6 +18,7 @@ export default function Home({ data_us_netfilx }) {
   if (DataShowtime) {var imagesShowtime = DataShowtime.map(item => item.posterURLs['500']);}
   if (DataApple) {var imagesApple = DataApple.map(item => item.posterURLs['500']);}
   if (DataMubi) {var imagesMubi = DataMubi.map(item => item.posterURLs['500']);}
+
   return (
     <>
       <div>
@@ -140,4 +36,93 @@ export default function Home({ data_us_netfilx }) {
       </div>
     </>
   )
+}
+
+export async function getStaticProps() {
+
+  const country = 'us';
+
+  const serviceByCountry = await fetch_service_by_country();
+
+  if (serviceByCountry?.netflix?.includes(country)) {
+    const array1 = await fetch_by_country_service_page(`${baseURL}/search/pro`, country, 'netflix', 1);
+    const array2 = await fetch_by_country_service_page(`${baseURL}/search/pro`, country, 'netflix', 2);
+    var DataNetflix = array1.concat(array2);
+  }
+
+  if (serviceByCountry?.prime?.includes(country)) {
+    const array1 = await fetch_by_country_service_page(`${baseURL}/search/pro`, country, 'prime', 1);
+    const array2 = await fetch_by_country_service_page(`${baseURL}/search/pro`, country, 'prime', 2);
+    var DataPrime = array1.concat(array2);
+  }
+
+  if (serviceByCountry?.disney?.includes(country)) {
+    const array1 = await fetch_by_country_service_page(`${baseURL}/search/pro`, country, 'disney', 1);
+    const array2 = await fetch_by_country_service_page(`${baseURL}/search/pro`, country, 'disney', 2);
+    var DataDisney = array1.concat(array2);
+  }
+
+  if (serviceByCountry?.hbo?.includes(country)) {
+    const array1 = await fetch_by_country_service_page(`${baseURL}/search/pro`, country, 'hbo', 1);
+    const array2 = await fetch_by_country_service_page(`${baseURL}/search/pro`, country, 'hbo', 2);
+    var DataHbo= array1.concat(array2);
+  }
+
+  if (serviceByCountry?.hulu?.includes(country)) {
+    const array1 = await fetch_by_country_service_page(`${baseURL}/search/pro`, country, 'hulu', 1);
+    const array2 = await fetch_by_country_service_page(`${baseURL}/search/pro`, country, 'hulu', 2);
+    var DataHulu= array1.concat(array2);
+  }
+
+  if (serviceByCountry?.peacock?.includes(country)) {
+    const array1 = await fetch_by_country_service_page(`${baseURL}/search/pro`, country, 'peacock', 1);
+    const array2 = await fetch_by_country_service_page(`${baseURL}/search/pro`, country, 'peacock', 2);
+    var DataPeacock= array1.concat(array2);
+  }
+
+  if (serviceByCountry?.paramount?.includes(country)) {
+    const array1 = await fetch_by_country_service_page(`${baseURL}/search/pro`, country, 'paramount', 1);
+    const array2 = await fetch_by_country_service_page(`${baseURL}/search/pro`, country, 'paramount', 2);
+    var DataParamount= array1.concat(array2);
+  }
+
+  if (serviceByCountry?.starz?.includes(country)) {
+    const array1 = await fetch_by_country_service_page(`${baseURL}/search/pro`, country, 'starz', 1);
+    const array2 = await fetch_by_country_service_page(`${baseURL}/search/pro`, country, 'starz', 2);
+    var DataStarz= array1.concat(array2);
+  }
+
+  if (serviceByCountry?.showtime?.includes(country)) {
+    const array1 = await fetch_by_country_service_page(`${baseURL}/search/pro`, country, 'showtime', 1);
+    const array2 = await fetch_by_country_service_page(`${baseURL}/search/pro`, country, 'showtime', 2);
+    var DataShowtime= array1.concat(array2);
+  }
+
+  if (serviceByCountry?.apple?.includes(country)) {
+    const array1 = await fetch_by_country_service_page(`${baseURL}/search/pro`, country, 'apple', 1);
+    const array2 = await fetch_by_country_service_page(`${baseURL}/search/pro`, country, 'apple', 2);
+    var DataApple= array1.concat(array2);
+  }
+
+  if (serviceByCountry?.mubi?.includes(country)) {
+    const array1 = await fetch_by_country_service_page(`${baseURL}/search/pro`, country, 'mubi', 1);
+    const array2 = await fetch_by_country_service_page(`${baseURL}/search/pro`, country, 'mubi', 2);
+    var DataMubi= array1.concat(array2);
+  }
+
+  return {
+    props: {
+      DataNetflix: DataNetflix,
+      DataPrime: DataPrime,
+      DataDisney: DataDisney,
+      DataHbo: DataHbo,
+      DataHulu: DataHulu,
+      DataPeacock: DataPeacock,
+      DataParamount: DataParamount,
+      DataStarz: DataStarz,
+      DataShowtime: DataShowtime,
+      DataApple: DataApple,
+      DataMubi: DataMubi
+    }
+  };
 }
