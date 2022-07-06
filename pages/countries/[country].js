d32cc8491d5b7fdd43fb3678188eb5c8ab0ddaf3
Livy <42135339+Livy-17@users.noreply.github.com>
2022-07-06 01:31:53 +0100

    1

--- pages/countries/[country].js
+++ pages/countries/[country].js
@@ -9,24 +9,22 @@ export default function Home({ DataNetflix, DataPrime, DataDisney, DataHbo, Data
 
   if (DataNetflix) {var imagesNetflix = DataNetflix.map(item => item.posterURLs['500']);}
   if (DataPrime) {var imagesPrime = DataPrime.map(item => item.posterURLs['500']);}
-  if (DataDisney) {var imagesDisney = DataDisney.map(item => item.posterURLs['500']);}
-  if (DataHbo) {var imagesHbo = DataHbo.map(item => item.posterURLs['500']);}
-  if (DataHulu) {var imagesHulu = DataHulu.map(item => item.posterURLs['500']);}
-  if (DataPeacock) {var imagesPeacock = DataPeacock.map(item => item.posterURLs['500']);}
-  if (DataParamount) {var imagesParamount = DataParamount.map(item => item.posterURLs['500']);}
-  if (DataStarz) {var imagesStarz = DataStarz.map(item => item.posterURLs['500']);}
-  if (DataShowtime) {var imagesShowtime = DataShowtime.map(item => item.posterURLs['500']);}
-  if (DataApple) {var imagesApple = DataApple.map(item => item.posterURLs['500']);}
-  if (DataMubi) {var imagesMubi = DataMubi.map(item => item.posterURLs['500']);}
-
-  console.log('123')
+  // if (DataDisney) {var imagesDisney = DataDisney.map(item => item.posterURLs['500']);}
+  // if (DataHbo) {var imagesHbo = DataHbo.map(item => item.posterURLs['500']);}
+  // if (DataHulu) {var imagesHulu = DataHulu.map(item => item.posterURLs['500']);}
+  // if (DataPeacock) {var imagesPeacock = DataPeacock.map(item => item.posterURLs['500']);}
+  // if (DataParamount) {var imagesParamount = DataParamount.map(item => item.posterURLs['500']);}
+  // if (DataStarz) {var imagesStarz = DataStarz.map(item => item.posterURLs['500']);}
+  // if (DataShowtime) {var imagesShowtime = DataShowtime.map(item => item.posterURLs['500']);}
+  // if (DataApple) {var imagesApple = DataApple.map(item => item.posterURLs['500']);}
+  // if (DataMubi) {var imagesMubi = DataMubi.map(item => item.posterURLs['500']);}
 
   return (
     <>
       <div>
         { imagesNetflix && <CarouselSub images={imagesNetflix} serviceName='Netflix' /> }
         { imagesPrime && <CarouselSub images={imagesPrime} serviceName='Prime' /> }
-        { imagesDisney && <CarouselSub images={imagesDisney} serviceName='Disney' /> }
+        {/* { imagesDisney && <CarouselSub images={imagesDisney} serviceName='Disney' /> }
         { imagesHbo && <CarouselSub images={imagesHbo} serviceName='HBO' /> }
         { imagesHulu && <CarouselSub images={imagesHulu} serviceName='Hulu' /> }
         { imagesPeacock && <CarouselSub images={imagesPeacock} serviceName='Peacock' /> }
@@ -34,7 +32,7 @@ export default function Home({ DataNetflix, DataPrime, DataDisney, DataHbo, Data
         { imagesStarz && <CarouselSub images={imagesStarz} serviceName='Starz' /> }
         { imagesShowtime && <CarouselSub images={imagesShowtime} serviceName='Showtime' /> }
         { imagesApple && <CarouselSub images={imagesApple} serviceName='Apple' /> }
-        { imagesMubi && <CarouselSub images={imagesMubi} serviceName='Mubi' /> }
+        { imagesMubi && <CarouselSub images={imagesMubi} serviceName='Mubi' /> } */}
       </div>
     </>
   )
@@ -91,91 +89,91 @@ export async function getStaticProps({ params: { country }}) {
     var DataPrime = null;
   }
 
-  if (serviceByCountry?.disney?.includes(country)) {
-    const array1 = await fetch_by_country_service_page(`${baseURL}/search/pro`, country, 'disney', 1);
-    const array2 = await fetch_by_country_service_page(`${baseURL}/search/pro`, country, 'disney', 2);
-    var DataDisney = array1.concat(array2);
-  } else {
-    var DataDisney = null;
-  }
+  // if (serviceByCountry?.disney?.includes(country)) {
+  //   const array1 = await fetch_by_country_service_page(`${baseURL}/search/pro`, country, 'disney', 1);
+  //   const array2 = await fetch_by_country_service_page(`${baseURL}/search/pro`, country, 'disney', 2);
+  //   var DataDisney = array1.concat(array2);
+  // } else {
+  //   var DataDisney = null;
+  // }
 
-  if (serviceByCountry?.hbo?.includes(country)) {
-    const array1 = await fetch_by_country_service_page(`${baseURL}/search/pro`, country, 'hbo', 1);
-    const array2 = await fetch_by_country_service_page(`${baseURL}/search/pro`, country, 'hbo', 2);
-    var DataHbo = array1.concat(array2);
-  } else {
-    var DataHbo = null;
-  }
+  // if (serviceByCountry?.hbo?.includes(country)) {
+  //   const array1 = await fetch_by_country_service_page(`${baseURL}/search/pro`, country, 'hbo', 1);
+  //   const array2 = await fetch_by_country_service_page(`${baseURL}/search/pro`, country, 'hbo', 2);
+  //   var DataHbo = array1.concat(array2);
+  // } else {
+  //   var DataHbo = null;
+  // }
 
-  if (serviceByCountry?.hulu?.includes(country)) {
-    const array1 = await fetch_by_country_service_page(`${baseURL}/search/pro`, country, 'hulu', 1);
-    const array2 = await fetch_by_country_service_page(`${baseURL}/search/pro`, country, 'hulu', 2);
-    var DataHulu = array1.concat(array2);
-  } else {
-    var DataHulu = null;
-  }
+  // if (serviceByCountry?.hulu?.includes(country)) {
+  //   const array1 = await fetch_by_country_service_page(`${baseURL}/search/pro`, country, 'hulu', 1);
+  //   const array2 = await fetch_by_country_service_page(`${baseURL}/search/pro`, country, 'hulu', 2);
+  //   var DataHulu = array1.concat(array2);
+  // } else {
+  //   var DataHulu = null;
+  // }
 
-  if (serviceByCountry?.peacock?.includes(country)) {
-    const array1 = await fetch_by_country_service_page(`${baseURL}/search/pro`, country, 'peacock', 1);
-    const array2 = await fetch_by_country_service_page(`${baseURL}/search/pro`, country, 'peacock', 2);
-    var DataPeacock = array1.concat(array2);
-  } else {
-    var DataPeacock = null;
-  }
+  // if (serviceByCountry?.peacock?.includes(country)) {
+  //   const array1 = await fetch_by_country_service_page(`${baseURL}/search/pro`, country, 'peacock', 1);
+  //   const array2 = await fetch_by_country_service_page(`${baseURL}/search/pro`, country, 'peacock', 2);
+  //   var DataPeacock = array1.concat(array2);
+  // } else {
+  //   var DataPeacock = null;
+  // }
 
-  if (serviceByCountry?.paramount?.includes(country)) {
-    const array1 = await fetch_by_country_service_page(`${baseURL}/search/pro`, country, 'paramount', 1);
-    const array2 = await fetch_by_country_service_page(`${baseURL}/search/pro`, country, 'paramount', 2);
-    var DataParamount = array1.concat(array2);
-  } else {
-    var DataParamount = null;
-  }
+  // if (serviceByCountry?.paramount?.includes(country)) {
+  //   const array1 = await fetch_by_country_service_page(`${baseURL}/search/pro`, country, 'paramount', 1);
+  //   const array2 = await fetch_by_country_service_page(`${baseURL}/search/pro`, country, 'paramount', 2);
+  //   var DataParamount = array1.concat(array2);
+  // } else {
+  //   var DataParamount = null;
+  // }
 
-  if (serviceByCountry?.starz?.includes(country)) {
-    const array1 = await fetch_by_country_service_page(`${baseURL}/search/pro`, country, 'starz', 1);
-    const array2 = await fetch_by_country_service_page(`${baseURL}/search/pro`, country, 'starz', 2);
-    var DataStarz = array1.concat(array2);
-  } else {
-    var DataStarz = null;
-  }
+  // if (serviceByCountry?.starz?.includes(country)) {
+  //   const array1 = await fetch_by_country_service_page(`${baseURL}/search/pro`, country, 'starz', 1);
+  //   const array2 = await fetch_by_country_service_page(`${baseURL}/search/pro`, country, 'starz', 2);
+  //   var DataStarz = array1.concat(array2);
+  // } else {
+  //   var DataStarz = null;
+  // }
 
-  if (serviceByCountry?.showtime?.includes(country)) {
-    const array1 = await fetch_by_country_service_page(`${baseURL}/search/pro`, country, 'showtime', 1);
-    const array2 = await fetch_by_country_service_page(`${baseURL}/search/pro`, country, 'showtime', 2);
-    var DataShowtime = array1.concat(array2);
-  } else {
-    var DataShowtime = null;
-  }
+  // if (serviceByCountry?.showtime?.includes(country)) {
+  //   const array1 = await fetch_by_country_service_page(`${baseURL}/search/pro`, country, 'showtime', 1);
+  //   const array2 = await fetch_by_country_service_page(`${baseURL}/search/pro`, country, 'showtime', 2);
+  //   var DataShowtime = array1.concat(array2);
+  // } else {
+  //   var DataShowtime = null;
+  // }
 
-  if (serviceByCountry?.apple?.includes(country)) {
-    const array1 = await fetch_by_country_service_page(`${baseURL}/search/pro`, country, 'apple', 1);
-    const array2 = await fetch_by_country_service_page(`${baseURL}/search/pro`, country, 'apple', 2);
-    var DataApple = array1.concat(array2);
-  } else {
-    var DataApple = null;
-  }
+  // if (serviceByCountry?.apple?.includes(country)) {
+  //   const array1 = await fetch_by_country_service_page(`${baseURL}/search/pro`, country, 'apple', 1);
+  //   const array2 = await fetch_by_country_service_page(`${baseURL}/search/pro`, country, 'apple', 2);
+  //   var DataApple = array1.concat(array2);
+  // } else {
+  //   var DataApple = null;
+  // }
 
-  if (serviceByCountry?.mubi?.includes(country)) {
-    const array1 = await fetch_by_country_service_page(`${baseURL}/search/pro`, country, 'mubi', 1);
-    const array2 = await fetch_by_country_service_page(`${baseURL}/search/pro`, country, 'mubi', 2);
-    var DataMubi = array1.concat(array2);
-  } else {
-    var DataMubi = null;
-  }
+  // if (serviceByCountry?.mubi?.includes(country)) {
+  //   const array1 = await fetch_by_country_service_page(`${baseURL}/search/pro`, country, 'mubi', 1);
+  //   const array2 = await fetch_by_country_service_page(`${baseURL}/search/pro`, country, 'mubi', 2);
+  //   var DataMubi = array1.concat(array2);
+  // } else {
+  //   var DataMubi = null;
+  // }
 
   return {
     props: {
       DataNetflix: DataNetflix,
       DataPrime: DataPrime,
-      DataDisney: DataDisney,
-      DataHbo: DataHbo,
-      DataHulu: DataHulu,
-      DataPeacock: DataPeacock,
-      DataParamount: DataParamount,
-      DataStarz: DataStarz,
-      DataShowtime: DataShowtime,
-      DataApple: DataApple,
-      DataMubi: DataMubi
+      // DataDisney: DataDisney,
+      // DataHbo: DataHbo,
+      // DataHulu: DataHulu,
+      // DataPeacock: DataPeacock,
+      // DataParamount: DataParamount,
+      // DataStarz: DataStarz,
+      // DataShowtime: DataShowtime,
+      // DataApple: DataApple,
+      // DataMubi: DataMubi
     }
   };
 }
